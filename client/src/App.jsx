9db340984f4cef99db3b71b99734d5c78a6ae03b
mashadesignworld <njugunak.kevin@gmail.com>
2024-06-06 12:44:32 +0300

    3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,30 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
-import './index.css'
-import Signup from './components/Signup'
-import Login from './components/Login'
-import ForgotPassword from './components/ForgotPassword'
-import ResetPassword from './components/ResetPassword'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import './index.css';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import ForgotPassword from './components/ForgotPassword';
+import ResetPassword from './components/ResetPassword';
 
 function App() {
-
-
   return (
     <BrowserRouter>
-
       <Routes>
+        {/* Redirect the root path to /signup */}
+        <Route path="/" element={<Navigate to="/signup" />} />
 
-        <Route path="/signup" element={<Signup />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/forgotpassword" element={<ForgotPassword />}></Route>
-        <Route path="/resetpassword/:token" element={<ResetPassword />}></Route>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/resetpassword/:token" element={<ResetPassword />} />
+
+        {/* Redirect any other path to /signup */}
+        <Route path="*" element={<Navigate to="/signup" />} />
       </Routes>
-
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
